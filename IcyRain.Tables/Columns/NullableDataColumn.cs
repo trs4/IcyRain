3fd9bcd5b9983c454048cf8b09bc15597e7552e6
trs4 <78437306+trs4@users.noreply.github.com>
2024-03-17 15:12:12 +0300

    GetNonNullableValues

--- IcyRain.Tables/Columns/NullableDataColumn.cs
+++ IcyRain.Tables/Columns/NullableDataColumn.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.Serialization;
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace IcyRain.Tables;
 
@@ -9,6 +11,98 @@ public abstract class NullableDataColumn<T> : SingleDataColumn<T?>
     protected NullableDataColumn(int capacity) : base(capacity) { }
 
     public sealed override bool IsNullable => true;
+
+    public HashSet<T> GetNonNullableUniqueValues(int count)
+    {
+        if (count < 0)
+            throw new ArgumentNullException(nameof(count));
+
+        if (Values is null)
+            return Fallback.HasValue ? [Fallback.Value] : [];
+
+        int valuesCount = Values.Count;
+        var values = new HashSet<T>(count);
+
+        if (count > valuesCount)
+        {
+            for (int i = 0; i < Values.Count; i++)
+            {
+                var value = Values[i];
+
+                if (value.HasValue)
+                    values.Add(value.Value);
+            }
+
+            if (Fallback.HasValue)
+                values.Add(Fallback.Value);
+        }
+        else
+        {
+            for (int i = 0; i < count; i++)
+            {
+                var value = Values[i];
+
+                if (value.HasValue)
+                    values.Add(value.Value);
+            }
+        }
+
+        return values;
+    }
+
+    public List<T> GetNonNullableValues(int count)
+    {
+        if (count < 0)
+            throw new ArgumentNullException(nameof(count));
+
+        if (Values is null)
+            return GetNullValues(count);
+
+        int valuesCount = Values.Count;
+        var values = new List<T>(count);
+
+        if (count > valuesCount)
+        {
+            for (int i = 0; i < Values.Count; i++)
+            {
+                var value = Values[i];
+
+                if (value.HasValue)
+                    values.Add(value.Value);
+            }
+
+            if (Fallback.HasValue)
+            {
+                for (int i = valuesCount; i < count; i++)
+                    values.Add(Fallback.Value);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < count; i++)
+            {
+                var value = Values[i];
+
+                if (value.HasValue)
+                    values.Add(value.Value);
+            }
+        }
+
+        return values;
+    }
+
+    private List<T> GetNullValues(int count)
+    {
+        if (!Fallback.HasValue)
+            return [];
+
+        var values = new List<T>(count);
+
+        for (int i = 0; i < count; i++)
+            values.Add(Fallback.Value);
+
+        return values;
+    }
 
     protected sealed override bool IsDefault(T? value) => !value.HasValue;
 
--- IcyRain.Tables/Columns/StructDataColumn.cs
+++ IcyRain.Tables/Columns/StructDataColumn.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.Serialization;
 
 namespace IcyRain.Tables;
@@ -8,6 +9,33 @@ public abstract class StructDataColumn<T> : SingleDataColumn<T>
     where T : struct, IEquatable<T>
 {
     protected StructDataColumn(int capacity) : base(capacity) { }
+
+    public HashSet<T> GetUniqueValues(int count)
+    {
+        if (count < 0)
+            throw new ArgumentNullException(nameof(count));
+
+        if (Values is null)
+            return [Fallback];
+
+        int valuesCount = Values.Count;
+        var values = new HashSet<T>(count);
+
+        if (count > valuesCount)
+        {
+            for (int i = 0; i < Values.Count; i++)
+                values.Add(Values[i]);
+
+            values.Add(Fallback);
+        }
+        else
+        {
+            for (int i = 0; i < count; i++)
+                values.Add(Values[i]);
+        }
+
+        return values;
+    }
 
     protected sealed override bool Equals(T x, T y) => x.Equals(y);
 
