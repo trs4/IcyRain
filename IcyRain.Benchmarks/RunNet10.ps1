b75ee2c70879112504378326e9c2e6cb602c5612
Алексей Михеев <amikheev45@yandex.ru>
2025-11-29 16:31:14 +0300

    net10.0

---
+++ IcyRain.Benchmarks/RunNet10.ps1
@@ -0,0 +1,2 @@
+﻿dotnet run -c Release -f net10.0 --runtimes net10.0 --join
+Read-Host -Prompt "Press Enter to exit"
--- IcyRain.Benchmarks/RunNet9.ps1
+++
@@ -1,2 +0,0 @@
-﻿dotnet run -c Release -f net9.0 --runtimes net9.0 --join
-Read-Host -Prompt "Press Enter to exit"
